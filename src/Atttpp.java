b89dd374867bc3e7f3e384f2aa267189cad1effa
Thuong Em <thuongtran160101@gmail.com>
2023-11-30 10:46:36 +0700

    init

---
+++ src/Atttpp.java
@@ -0,0 +1,10 @@
+public class Atttpp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        mainHIHI(args);
+    }
+
+    public static void mainHIHI(String[] args) {
+        System.out.println("Helloadadad, World!");
+    }
+}
